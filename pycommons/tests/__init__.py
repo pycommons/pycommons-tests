7891ccf65e308ad8a8dfab9b918cf0d7bfe4eba9
Shashank Sharma <shashankrnr32@gmail.com>
2023-07-13 20:55:18 +0530

    Add docstrings, Fix relative paths

--- pycommons/tests/__init__.py
+++ pycommons/tests/__init__.py
@@ -6,6 +6,29 @@ from importlib_metadata import PackageNotFoundError, version
 __author__ = "Shashank Sharma"
 __email__ = "shashankrnr32@gmail.com"
 
+from .case import IsolatedAsyncIOTestCase, TestCase
+from .condition import skip, skip_if, skip_unless, flaky, fails
+from .exception import raises
+from .parametrized import cases, TestData
+from .utils import TestUtils
+
+__all__ = [
+    "IsolatedAsyncIOTestCase",
+    "TestCase",
+    "TestData",
+    "TestUtils",
+    "__author__",
+    "__email__",
+    "__version__",
+    "cases",
+    "fails",
+    "flaky",
+    "raises",
+    "skip",
+    "skip_if",
+    "skip_unless",
+]
+
 # Used to automatically set version number from GitHub actions
 # as well as not break when being tested locally
 try:
--- pycommons/tests/case.py
+++ pycommons/tests/case.py
@@ -5,14 +5,57 @@ from pycommons.tests.matcher import Matcher
 
 
 class TestCase(unittest.TestCase):
-    def assertSame(self, expected: Any, actual: Any, msg: Optional[str] = None):
+    """
+    Extensions to python's `unittest.TestCase` class. Provides additional
+    assertion methods and helper methods.
+    """
+
+    def assertSame(self, expected: Any, actual: Any, msg: Optional[str] = None) -> None:
+        """
+        Assert if the expected object is same as the actual object, i.e. their ids match.
+
+        Args:
+            expected: Expected object
+            actual: Actual object
+            msg: message
+
+        Returns:
+            None
+        """
         if id(expected) != id(actual):
             self.fail(msg)
 
     def assertNotSame(self, expected: Any, actual: Any, msg: Optional[str] = None):
+        """
+       Assert if the expected object is not the same as the actual object, i.e. their ids do not match.
+
+       Args:
+           expected: Expected object
+           actual: Actual object
+           msg: message
+
+       Returns:
+           None
+       """
         if id(expected) == id(actual):
             self.fail(msg)
 
     def assertThat(self, matcher: Matcher[Any], actual: Any, msg: Optional[str] = None):
+        """
+        Provides matcher type assertion. A matcher is a functional interface with a single abstract method
+        `match` that takes an object (`actual`) and returns True if the matching is successful.
+
+        Args:
+            matcher: A matcher object
+            actual: The object to be matched
+            msg: message
+
+        Returns:
+            None
+        """
         if not matcher.match(actual):
             self.fail(msg)
+
+
+class IsolatedAsyncIOTestCase(unittest.IsolatedAsyncioTestCase, TestCase):
+    ...
